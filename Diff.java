2906024ea68b88715feea578a1b0e396d82cf8f7
Abhijeet Sen <abhijeetsen661@gmail.com>
2024-11-01 11:53:46 +0530

    changes commited

--- Diff.java
+++ Diff.java
@@ -1,7 +1,7 @@
 
 public class Diff{
     public static void main(String[] args){
-        int a=5,b=6;
-     System.out.println("The sum is:"+ (a*b));
+        int a=5,b=6,c=8;
+     System.out.println("The sum is:"+ (a*b+c));
     }
     }
